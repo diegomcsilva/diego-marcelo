013ddf970cf98da61037cc82fe6665487ee571c5
Diego Marcelo Costa da Silva <diego.marcelo@g.globo>
2021-10-10 01:21:37 -0300

    Add slick carousel in projcet

--- pages/components/Footer/Footer.tsx
+++ pages/components/Footer/Footer.tsx
@@ -1,18 +1,34 @@
+// Ref to slider - https://react-slick.neostack.com/
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import Slider from 'react-slick'
 
 import { LANGUAGES } from '../../../utils/contants'
 import styles from './Footer.module.css'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const Footer: NextPage = () => {
+
+  var settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    // centerMode: true,
+  };
+
   return (
     <footer className={styles.footer}>
-      {console.log({ LANGUAGES })}
-      {LANGUAGES.map(language =>
-        <span key={language} className={styles.footerItem}>
-          <Image key={language} src={`/icons-languages/${language}.png`} alt={language} width={50} height={50}/>
-        </span>
-      )}
+      <Slider {...settings}>
+        {LANGUAGES.map(language =>
+          <span key={language} className={styles.footerItem}>
+            <Image key={language} title={language} src={`/icons-languages/${language}.png`} alt={language} width={50} height={50} objectFit='contain'/>
+          </span>
+        )}
+      </Slider>
     </footer>
   )
 }
